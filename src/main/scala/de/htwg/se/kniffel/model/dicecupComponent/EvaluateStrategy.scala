25ad72c169b314993a0920fe95f40301a3d3cef1
benediktscheffel <benedikt.scheffel@hotmail.de>
2023-10-30 14:04:55 +0100

    fix emptyList bug when evaluating result

--- src/main/scala/de/htwg/se/kniffel/model/dicecupComponent/EvaluateStrategy.scala
+++ src/main/scala/de/htwg/se/kniffel/model/dicecupComponent/EvaluateStrategy.scala
@@ -10,17 +10,17 @@ object EvaluateStrategy {
 
   def checkBigStreet(data: List[Int]) : Boolean = mapToFrequency(data).max == 1 & data.max - data.min == 4
 
-  def threeOfAKind(data: List[Int]): Int = getSum(data, mapToFrequency(data).max >= 3)
+  def threeOfAKind(data: List[Int]): Int = if (data.nonEmpty) getSum(data, mapToFrequency(data).max >= 3) else 0
 
-  def fourOfAKind(data: List[Int]): Int = getSum(data, mapToFrequency(data).max >= 4)
+  def fourOfAKind(data: List[Int]): Int = if (data.nonEmpty) getSum(data, mapToFrequency(data).max >= 4) else 0
 
-  def fullHouse(data: List[Int]): Int = if (mapToFrequency(data).max == 3 & mapToFrequency(data).min == 2) 25 else 0
+  def fullHouse(data: List[Int]): Int = if (data.nonEmpty && (mapToFrequency(data).max == 3 & mapToFrequency(data).min == 2)) 25 else 0
 
-  def bigStreet(data: List[Int]): Int = if (mapToFrequency(data).max == 1 & data.max - data.min == 4) 40 else 0
+  def bigStreet(data: List[Int]): Int = if (data.nonEmpty && (mapToFrequency(data).max == 1 & data.max - data.min == 4)) 40 else 0
 
-  def smallStreet(data: List[Int]): Int = if (checkBigStreet(data) | data.distinct.size == 4 & data.distinct.max - data.distinct.min == 3 | data.distinct.sum.equals(19) | data.distinct.sum.equals(16)) 30 else 0
+  def smallStreet(data: List[Int]): Int = if (data.nonEmpty && (checkBigStreet(data) | data.distinct.size == 4 & data.distinct.max - data.distinct.min == 3 | data.distinct.sum.equals(19) | data.distinct.sum.equals(16))) 30 else 0
 
-  def kniffel(data: List[Int]): Int = if (mapToFrequency(data).max == 5) 50 else 0
+  def kniffel(data: List[Int]): Int = if (data.nonEmpty && mapToFrequency(data).max == 5) 50 else 0
 
   def sum(data: List[Int]): Int = data.sum
 
