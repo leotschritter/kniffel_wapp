8004e548eb5892a196e756fa6212a59f92c7134c
Leo Tschritter <leotschritter@gmail.com>
2023-11-14 08:54:56 +0100

    everything bootstrap now!

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,30 +1,6 @@
-let modal = document.getElementById("myModal");
-let btn = document.getElementsByClassName("startButton")[0];
-let span = document.getElementsByClassName("close")[0];
-let btnNo = document.getElementById("no");
-
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-span.onclick = function () {
-    modal.style.display = "none";
-}
-
-btnNo.onclick = function () {
-    modal.style.display = "none";
-}
-
-window.onclick = function (event) {
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-}
-
 function startGame() {
     const numberOfPlayers = document.getElementById("numberOfPlayers").value;
     if (numberOfPlayers > 8 || numberOfPlayers < 2) {
-        modal.style.display = "none";
         alert("Please only select a value between 2 and 8!");
         return;
     }
