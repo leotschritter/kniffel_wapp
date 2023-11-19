345f18a9f229cbb346b86eea5153cd511631f07c
Leo Tschritter <leotschritter@gmail.com>
2023-11-19 16:44:30 +0100

    All events are now controlled within js files

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,17 @@
-function startGame() {
-    const numberOfPlayers = document.getElementById("numberOfPlayers").value;
-    if (numberOfPlayers > 8 || numberOfPlayers < 2) {
-        alert("Please only select a value between 2 and 8!");
-        return;
-    }
-    fetch('/new?players=' + numberOfPlayers).then(response => {
-        if (!response.ok) {
-            console.error("Failed starting new game!");
-        } else {
-            window.location.href = '/kniffel';
+$(document).ready(function () {
+    const btnStart = document.getElementById('startGameButton');
+    btnStart.onclick = function () {
+        const numberOfPlayers = document.getElementById("numberOfPlayers").value;
+        if (numberOfPlayers > 8 || numberOfPlayers < 2) {
+            alert("Please only select a value between 2 and 8!");
+            return;
         }
-    });
-}
+        fetch('/new?players=' + numberOfPlayers).then(response => {
+            if (!response.ok) {
+                console.error("Failed starting new game!");
+            } else {
+                window.location.href = '/kniffel';
+            }
+        });
+    };
+});
